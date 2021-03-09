d87f4c18ae660f1c257f8912f09d2737ca694164
Andrew <ayang24@email.unc.edu>
2021-03-08 23:34:18 -0500

    added join-the-team page outline

--- mc-app/src/components/JoinTheTeam.jsx
+++ mc-app/src/components/JoinTheTeam.jsx
@@ -1,8 +1,20 @@
 import React, {} from 'react';
+import '../styles/JoinTheTeam.css';
 
 const JoinTheTeam = () => {
     return (<>
-    Join The Team page
+        <div class="imageContainer">
+            <div class="centered">
+                <h1>Join The Team</h1>
+            </div>
+        
+        </div>
+        
+        <div className="team-content">
+            <p>The Meantime Coffee Co. is a non-profit, student-run coffee shop located in lobby of the Campus Y, UNC's Center for Social Justice and Social Innovation. To learn more about the organization, please explore our website or find us on Facebook, Twitter or Instagram.</p>
+            <br></br>
+            <p class="thick">Feel free to reach out with questions: marketing@meantimecoffee.com</p>
+        </div>
     </>)
 }
 
