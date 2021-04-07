1921035b7eddd1c7945f6ff975ed2f2c1feba0b7
sgo21 <54879349+sgo21@users.noreply.github.com>
2021-04-07 13:24:06 -0400

    Set up contact form structure and styling

--- mc-app/src/components/OurImpact.jsx
+++ mc-app/src/components/OurImpact.jsx
@@ -19,15 +19,19 @@ const OurImpact = () => {
             <div className="impact-grid">
                 <div className="column">
                     <h2 className="futura-med">Professional Development</h2>
-                    <p className="futura-book">The Meantime is entirely student-run, from barista to CEO. We create <strong>hands-on business experience</strong> not offered by similar on-campus enterprises. Everyone joins our team as a barista with the opportunity to make coffee, create community, or develop the business.</p>
+                    <p className="futura-book">The Meantime is entirely student-run, from barista to CEO. We create <strong>hands-on business experience</strong> not offered by similar on-campus enterprises. 
+                        Everyone joins our team as a barista with the opportunity to make coffee, create community, or develop the business.</p>
                 </div>
                 <div className="column">
                     <h2 className="futura-med">Student-Driven Funding</h2>
-                    <p className="futura-book">The Meantime supports the thriving Carolina community it calls home by <strong>reinvesting profits into student scholarships and grants.</strong> We believe that our fellow Tar Heels can creatively impact this campus, so we invest in making their visions a reality.</p>
+                    <p className="futura-book">The Meantime supports the thriving Carolina community it calls home by <strong>reinvesting profits into student scholarships and grants.</strong> 
+                        We believe that our fellow Tar Heels can creatively impact this campus, so we invest in making their visions a reality.</p>
                 </div>
                 <div className="column">
                     <h2 className="futura-med">Sustainable Food Hub</h2>
-                    <p className="futura-book">We support other <strong>student food ventures and local businesses</strong> by providing a testing ground on campus. We sell baked goods from <a href="https://heellife.unc.edu/organization/thesondermarket"><em>The Sonder Market</em></a>, a student-run initiative that combats food insecurity in our community. We source our coffee from <a href="https://carrborocoffee.com/"><em>Carrboro Coffee Roasters</em></a>, a sustainable roastery just down the road.</p>
+                    <p className="futura-book">We support other <strong>student food ventures and local businesses</strong> by providing a testing ground on campus. 
+                        We sell baked goods from <a href="https://heellife.unc.edu/organization/thesondermarket"><em>The Sonder Market</em></a>, a student-run initiative that combats food insecurity in our community. 
+                        We source our coffee from <a href="https://carrborocoffee.com/"><em>Carrboro Coffee Roasters</em></a>, a sustainable roastery just down the road.</p>
                 </div>
             </div>
 
