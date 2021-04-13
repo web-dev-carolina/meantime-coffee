aaf7fe2f92e25cd7dcf3670840e32bd147c6a072
Adysen <adysen66@gmail.com>
2021-04-13 13:49:09 -0400

    News

--- mc-app/src/components/News.jsx
+++ mc-app/src/components/News.jsx
@@ -1,8 +1,30 @@
 import React, {} from 'react';
+import { Row, Container, Col } from 'react-bootstrap';
+
+
 
 const News = () => {
     return (<>
-    News page
+    <Container>
+        <Col>
+            <h1>CONGRATULATIONS TO THE MEANTIME EXECUTIVE TEAM!</h1>
+            <p>April 25, 2019</p>
+        </Col>
+        <Col>
+            <Row>
+                <Col>Image here</Col>
+                <Col>
+                <h2>Chief Executive Officer - Oznur Ikiz</h2>
+                <p>“Meantime shaped my whole experience at Carolina, and already gave me so much that I cannot possibly begin to pay back. I am incredibly honored and grateful to have the opportunity to serve and give back to this wonderful organization, its staff and community.”</p>
+                <p>Favorite drink: Drip coffee</p>
+                </Col>
+            </Row>
+        
+        
+        
+        
+        </Col>
+    </Container>
     </>)
 }
 
