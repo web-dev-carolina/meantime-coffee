1b9555e3cd89cce041a2dda9a133ed96bec7280e
Andrew <ayang24@email.unc.edu>
2021-04-21 02:06:04 -0400

    attempted footer

--- mc-app/src/components/JoinTheTeam.jsx
+++ mc-app/src/components/JoinTheTeam.jsx
@@ -5,15 +5,15 @@ const JoinTheTeam = () => {
     return (<>
         <div className="imageContainer">
             <div className="centered">
-                <h1>Join The Team</h1>
+                <h1 class="title">Join The Team</h1>
             </div>
         
         </div>
         
         <div className="team-content">
-            <p>The Meantime Coffee Co. is a non-profit, student-run coffee shop located in lobby of the Campus Y, UNC's Center for Social Justice and Social Innovation. To learn more about the organization, please explore our website or find us on Facebook, Twitter or Instagram.</p>
+            <p style={{textAlign: "left"}}>The Meantime Coffee Co. is a non-profit, student-run coffee shop located in lobby of the Campus Y, UNC's Center for Social Justice and Social Innovation. To learn more about the organization, please explore our website or find us on Facebook, Twitter or Instagram.</p>
             <br></br>
-            <p className="thick">Feel free to reach out with questions: marketing@meantimecoffee.com</p>
+            <p className="thick" style={{textAlign: "left"}}>Feel free to reach out with questions: marketing@meantimecoffee.com</p>
         </div>
     </>)
 }
