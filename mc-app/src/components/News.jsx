fb238dab8e417849a48a6bc283f2727391278177
Adysen <adysen66@gmail.com>
2021-05-04 16:53:07 -0400

    styling the news page

--- mc-app/src/components/News.jsx
+++ mc-app/src/components/News.jsx
@@ -2,6 +2,7 @@ import React, {} from 'react';
 import { Row, Container, Col } from 'react-bootstrap';
 import CustomNavbar from "./CustomNavbar.jsx";
 import '../styles/News.css';
+import '../App.css';
 import oz from '../assets/OznurIkiz.jpeg';
 import em from '../assets/ElizabethMoore.jpeg';
 import db from '../assets/DhvaniBhatia .jpeg';
@@ -16,10 +17,10 @@ const News = () => {
     return (<>
     <CustomNavbar title={"NEWS"}></CustomNavbar>
     <Container>
-        <Row>
+        <Row className="mainbox">
             <Col xs lg="4" className="leftBar">
-                <h1 className="leftBarTxt"><strong>CONGRATULATIONS TO THE MEANTIME EXECUTIVE TEAM!</strong></h1>
-                <p>April 25, 2019</p>
+                <h1 className="leftBarTxt futura-heavy"><strong>CONGRATULATIONS TO THE MEANTIME EXECUTIVE TEAM!</strong></h1>
+                <p className="futura-book larger-font">April 25, 2019</p>
             </Col>
             <Col xs lg="8" className="rightBar">
                 <Row className="execCard">
@@ -28,9 +29,9 @@ const News = () => {
                             <img className="execimg" src={oz}></img>
                         </div></Col>
                     <Col>
-                    <h2>Chief Executive Officer - Oznur Ikiz</h2>
-                    <p>“Meantime shaped my whole experience at Carolina, and already gave me so much that I cannot possibly begin to pay back. I am incredibly honored and grateful to have the opportunity to serve and give back to this wonderful organization, its staff and community.”</p>
-                    <p>Favorite drink: Drip coffee</p>
+                    <h2 className="futura-heavy">Chief Executive Officer - Oznur Ikiz</h2>
+                    <p className="futura-book larger-font">“Meantime shaped my whole experience at Carolina, and already gave me so much that I cannot possibly begin to pay back. I am incredibly honored and grateful to have the opportunity to serve and give back to this wonderful organization, its staff and community.”</p>
+                    <p className="futura-book larger-font">Favorite drink: Drip coffee</p>
                     </Col>
                 </Row>
 
@@ -41,9 +42,9 @@ const News = () => {
                             <img className="execimg" src={em}></img>
                         </div></Col>
                     <Col>
-                    <h2>Chief Human Resources Officer - Elizabeth Moore</h2>
-                    <p>“I love the community we've built around the people—from employees to customers—and the global impact.”</p>
-                    <p>Favorite drink: 8 oz. Americano no room</p>
+                    <h2 className="futura-heavy">Chief Human Resources Officer - Elizabeth Moore</h2>
+                    <p className="futura-book larger-font">“I love the community we've built around the people—from employees to customers—and the global impact.”</p>
+                    <p className="futura-book larger-font">Favorite drink: 8 oz. Americano no room</p>
                     </Col>
                 </Row>
 
@@ -53,9 +54,9 @@ const News = () => {
                             <img className="execimg" src={db}></img>
                         </div></Col>
                     <Col>
-                    <h2>Chief Marketing Officer - Dhvani Bhatia</h2>
-                    <p>“I want find new and cool ways to engage with our customers and to share our story to make sure the Meantime is known for more than its coffee.”</p>
-                    <p>Favorite drink: Iced lavender latte</p>
+                    <h2 className="futura-heavy">Chief Marketing Officer - Dhvani Bhatia</h2>
+                    <p className="futura-book larger-font">“I want find new and cool ways to engage with our customers and to share our story to make sure the Meantime is known for more than its coffee.”</p>
+                    <p className="futura-book larger-font">Favorite drink: Iced lavender latte</p>
                     </Col>
                 </Row>
 
@@ -65,9 +66,9 @@ const News = () => {
                             <img className="execimg" src={fz}></img>
                         </div></Col>
                     <Col>
-                    <h2>Chief Operations Officer - Fehmi Zengince</h2>
-                    <p>“The family atmosphere at Meantime is something that I'll cherish for the rest of my life.”</p>
-                    <p>Favorite drink: Oak milk hazelnut latte</p>
+                    <h2 className="futura-heavy">Chief Operations Officer - Fehmi Zengince</h2>
+                    <p className="futura-book larger-font">“The family atmosphere at Meantime is something that I'll cherish for the rest of my life.”</p>
+                    <p className="futura-book larger-font">Favorite drink: Oak milk hazelnut latte</p>
                     </Col>
                 </Row>
 
@@ -77,10 +78,10 @@ const News = () => {
                             <img className="execimg" src={gb}></img>
                         </div></Col>
                     <Col>
-                    <h2>Chief Financial Officer - Geoff Bell </h2>
-                    <p><i>Goeff will be serving as CFO for the rest of the 2018-19 school year.</i></p>
-                    <p>“My favorite part of The Meantime is that it is student-founded and student-operated. There aren’t many organizations with this sort of scale that can say that.”</p>
-                    <p>Favorite drink: Drip coffee with no cream or sugar</p>
+                    <h2 className="futura-heavy">Chief Financial Officer - Geoff Bell </h2>
+                    <p className="futura-book larger-font"><i>Goeff will be serving as CFO for the rest of the 2018-19 school year.</i></p>
+                    <p className="futura-book larger-font">“My favorite part of The Meantime is that it is student-founded and student-operated. There aren’t many organizations with this sort of scale that can say that.”</p>
+                    <p className="futura-book larger-font">Favorite drink: Drip coffee with no cream or sugar</p>
                     </Col>
                 </Row>
 
@@ -90,10 +91,10 @@ const News = () => {
                             <img className="execimg" src={jr}></img>
                         </div></Col>
                     <Col>
-                    <h2>Chief Financial Officer - Jack Robertson </h2>
-                    <p><i>Jack will take over as CFO in Fall 2019.</i></p>
-                    <p>Jack wants to continue making The Meantime a sustainable organization by developing its institutional memory.</p>
-                    <p>Favorite drink: London fog</p>
+                    <h2 className="futura-heavy">Chief Financial Officer - Jack Robertson </h2>
+                    <p className="futura-book larger-font"><i>Jack will take over as CFO in Fall 2019.</i></p>
+                    <p className="futura-book larger-font">Jack wants to continue making The Meantime a sustainable organization by developing its institutional memory.</p>
+                    <p className="futura-book larger-font">Favorite drink: London fog</p>
                     </Col>
                 </Row>
             </Col>
